32563c653dcceb378191421deffe3a35c3f7f99f
poonam300796 <poonam300796@gmail.com>
2016-02-15 22:24:49 +0530

    Removed cout

--- MISSION_CODE_11/xor_fav.cpp
+++ MISSION_CODE_11/xor_fav.cpp
@@ -39,26 +39,28 @@ int func(int arr[],int rr ,int ll,int k,int n ,int m)
 int main()
 {
 	int n,m,arr[50],r[50],l[50],kk,count;
-	cout<<"\n Enter the size of array : ";
+	//cout<<"\n Enter the size of array : ";
 	cin>>n;
-	cout<<"\n Enter the total number  of query : ";
+	//cout<<"\n Enter the total number  of query : ";
 	cin>>m;
-	cout<<"\n Enter the elements of array \n";
+	//cout<<"\n Enter the elements of array \n";
 	for(int i=0;i<n;i++)
 		cin>>arr[i];
 	for(int j=0;j<m;j++)	
 	{
-		cout<<"\n Enter value or r & l of query "<<j+1<<" : ";
+	//	cout<<"\n Enter value or r & l of query "<<j+1<<" : ";
 		cin>>r[j]>>l[j];
 	}	
 	int k;
-	cout<<"\n Enter your favourite number : ";
+	//cout<<"\n Enter your favourite number : ";
 	cin>>k;
 	for(kk=0;kk<m;kk++)
 	{
-		cout<<"\n Solutions of query no. : "<<kk+1<<"\n";
+		//cout<<"\n Solutions of query no. : "
+	cout<<kk+1;//<<"\n";
 		count=func(arr, r[kk], l[kk], k,n,m);
-		cout<<"\n Total number of solutions  : "<<count;
+		//cout<<"\n Total number of solutions  : "
+	cout<<count;
 	}
 	cout<<"\n";
 	return 0;
